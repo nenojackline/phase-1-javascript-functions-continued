d2e6e8c7fdadba2d7bb53cab62571498184d07d4
jackline chepngeno <chepngenojackline0@gmail.com>
2022-06-08 10:21:10 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return (`This Saturday, I want to ${activity}!`);
+}
+saturdayFun()
+saturdayFun("bathe my dog")
+saturdayFun();
+saturdayFun("bathe my dog");
+
+function mondayWork(routine = "go to the office") {
+    return (`This Monday, I will ${routine}.`);
+}
+mondayWork();
+
+function wrapAdjective(cool = "*") {
+    return function(message = "special") {
+        return `You are ${cool}${message}${cool}!`;
+    }
+} 
+wrapAdjective("||");
